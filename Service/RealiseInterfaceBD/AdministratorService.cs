11f26bea921496d64f58ef4f9b39968a4e8faa2b
Dmitry <apelsinka.channel@gmail.com>
2018-05-28 23:37:39 +0400

    desc2

---
+++ Service/RealiseInterfaceBD/AdministratorService.cs
@@ -0,0 +1,95 @@
+﻿using Models;
+using Service.ConnectingModel;
+using Service.LogicInterface;
+using Service.UserViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.RealiseInterfaceBD
+{
+   public class CustomerServiceBD : IAdministratorService
+    {
+        private DBContext context;
+
+        public CustomerServiceBD(DBContext context)
+        {
+            this.context = context;
+        }
+
+        public List<AdministratorViewModel> GetList()
+        {
+            List<AdministratorViewModel> result = context.Administrators
+                .Select(rec => new AdministratorViewModel
+                {
+                    admin_id = rec.admin_id,
+                    name = rec.name,
+                    login = rec.login,
+                    password = rec.password
+                })
+                .ToList();
+            return result;
+        }
+
+        public AdministratorViewModel GetElement(int id)
+        {
+            Administrator element = context.Administrators.FirstOrDefault(rec => rec.admin_id == id);
+            if (element != null)
+            {
+                return new AdministratorViewModel
+                {
+                    admin_id = element.admin_id,
+                    name = element.name,
+                };
+            }
+            throw new Exception("Элемент не найден");
+        }
+
+        public void AddElement(AdministratorConnectingModel model)
+        {
+            Administrator element = context.Administrators.FirstOrDefault(rec => rec.name == model.name);
+            if (element != null)
+            {
+                throw new Exception("Уже есть клиент с таким ФИО");
+            }
+            context.Administrators.Add(new Administrator
+            {
+                name = model.name
+            });
+            context.SaveChanges();
+        }
+
+        public void UpdElement(AdministratorConnectingModel model)
+        {
+            Administrator element = context.Administrators.FirstOrDefault(rec =>
+                                    rec.name == model.name && rec.admin_id != model.admin_id);
+            if (element != null)
+            {
+                throw new Exception("Уже есть клиент с таким ФИО");
+            }
+            element = context.Administrators.FirstOrDefault(rec => rec.admin_id == model.admin_id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.name = model.name;
+            context.SaveChanges();
+        }
+
+        public void DelElement(int id)
+        {
+            Administrator element = context.Administrators.FirstOrDefault(rec => rec.admin_id == id);
+            if (element != null)
+            {
+                context.Administrators.Remove(element);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+    }
+}
---
+++ Service/RealiseInterfaceBD/SupplierService.cs
@@ -0,0 +1,93 @@
+﻿using Models;
+using Service.ConnectingModel;
+using Service.LogicInterface;
+using Service.UserViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.RealiseInterfaceBD
+{
+   public class WorkerServiceBD : ISupplierService
+    {
+        private DBContext context;
+
+        public WorkerServiceBD(DBContext context)
+        {
+            this.context = context;
+        }
+
+        public List<SupplierViewModel> GetList()
+        {
+            List<SupplierViewModel> result = context.Suppliers
+                .Select(rec => new SupplierViewModel
+                {
+                    supplier_id = rec.supplier_id,
+                    name = rec.name
+                })
+                .ToList();
+            return result;
+        }
+
+        public SupplierViewModel GetElement(int id)
+        {
+            Supplier element = context.Suppliers.FirstOrDefault(rec => rec.supplier_id == id);
+            if (element != null)
+            {
+                return new SupplierViewModel
+                {
+                    supplier_id = element.supplier_id,
+                    name = element.name
+                };
+            }
+            throw new Exception("Элемент не найден");
+        }
+
+        public void AddElement(SupplierConnectingModel model)
+        {
+            Supplier element = context.Suppliers.FirstOrDefault(rec => rec.name == model.name);
+            if (element != null)
+            {
+                throw new Exception("Уже есть сотрудник с таким ФИО");
+            }
+            context.Suppliers.Add(new Supplier
+            {
+                name = model.name
+            });
+            context.SaveChanges();
+        }
+
+        public void UpdElement(SupplierConnectingModel model)
+        {
+            Supplier element = context.Suppliers.FirstOrDefault(rec =>
+                                        rec.name == model.name && rec.supplier_id != model.supplier_id);
+            if (element != null)
+            {
+                throw new Exception("Уже есть сотрудник с таким ФИО");
+            }
+            element = context.Suppliers.FirstOrDefault(rec => rec.supplier_id == model.supplier_id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.name = model.name;
+            context.SaveChanges();
+        }
+
+        public void DelElement(int id)
+        {
+            Supplier element = context.Suppliers.FirstOrDefault(rec => rec.supplier_id == id);
+            if (element != null)
+            {
+                context.Suppliers.Remove(element);
+                context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+    }
+}
